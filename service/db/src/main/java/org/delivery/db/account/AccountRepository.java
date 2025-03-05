eff012c2e48306d831185aed860f7c490dbd2dcb
sohyeonjung <jhyeon23@konkuk.ac.kr>
2025-03-05 19:10:44 +0900

    first commit

---
+++ service/db/src/main/java/org/delivery/db/account/AccountRepository.java
@@ -0,0 +1,7 @@
+package org.delivery.db.account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface AccountRepository extends JpaRepository<AccountEntity, Long> {
+}
